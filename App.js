f9c4985c865a27faf7bca1053e8019b9e0832f4d
margoshkari <68592214+margoshkari@users.noreply.github.com>
2023-03-27 17:56:56 +0300

    init

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet ,Text, View, Button, Image} from 'react-native';
+import { Camera } from 'expo-camera';
+import { RadioButton } from 'react-native-paper';
+
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [checked, setChecked] = useState(false);
+useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+})();
+  }, []);
+const takePicture = async () => {
+    if(camera){
+        const data = await camera.takePictureAsync(null)
+        setImage(data.uri);
+    }
+  }
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+   <View style={{ flex: 1}}>
+      <View style={styles.cameraContainer}>
+            <Camera 
+            ref={ref => setCamera(ref)}
+            style={styles.fixedRatio} 
+            type={type}
+            ratio={'1:1'} />
+      </View>
+        <View style={styles.btn}>
+          <RadioButton
+          status={ checked === true ? 'checked' : 'unchecked' }
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+            setChecked(!checked)
+          }}
+        />
+        <Text onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+            setChecked(!checked)
+          }}>Flip</Text>
+        </View>
+        
+       <Button title="Take Picture" onPress={() => takePicture()} />
+        {image && <Image source={{uri: image}} style={{flex:1}}/>}
+   </View>
+  );
+}
+const styles = StyleSheet.create({
+  cameraContainer: {
+      flex: 1,
+      flexDirection: 'row'
+  },
+  fixedRatio:{
+      flex: 1,
+      aspectRatio: 1
+  },
+  btn:{
+    flexDirection: "row",
+    alignItems: 'center'
+  }
+})
